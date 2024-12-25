4c3c953b5528bc566fd353e5865ab5dab0b2c77d
Chuigda Whitegive <icey@icey.tech>
2024-12-25 23:24:41 +0800

    Add `DoubleBuffer`, remove `pl7`, update version

--- src/main/java/tech/icey/xjbutil/pl7/syntax/SourceLocation.java
+++
@@ -1,5 +0,0 @@
-package tech.icey.xjbutil.pl7.syntax;
-
-public record SourceLocation(String file, int line, int col) {
-    public static final SourceLocation DUMMY = new SourceLocation("", -1, -1);
-}
